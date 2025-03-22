cd2159063d64a5162b26249e85d0b3a38395a602
ecobarrios <ecobarrios2025@gmail.com>
2025-03-22 05:30:51 -0300

    ahora

--- app/api/news/[id]/route.ts
+++ app/api/news/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prismadb";
 
 export async function GET(
+  request: NextRequest,
   context: { params: { id: string } }
 ) {
   const { id } = context.params;
