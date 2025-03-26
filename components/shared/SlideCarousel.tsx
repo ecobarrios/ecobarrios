6ac75a74a8bd592db298d97d81ad7c048af4f5a0
ecobarrios <ecobarrios2025@gmail.com>
2025-03-26 11:15:31 -0300

    imagen carousel

--- components/shared/SlideCarousel.tsx
+++ components/shared/SlideCarousel.tsx
@@ -13,7 +13,7 @@ import { useRef } from "react";
 import slider1 from "@/public/PNG/slider_1.png";
 import slider2 from "@/public/PNG/slider_2.png";
 import slider3 from "@/public/PNG/slider_3.png";
-import imgBaseSlider from "@/public/SVG/image_base_button_inscription.svg"
+import imgBaseSlider from "@/public/SVG/image_base_button_inscription.svg";
 import Image from "next/image";
 function SlideCarousel() {
   const plugin = useRef(Autoplay({ delay: 2500, stopOnInteraction: false }));
@@ -25,37 +25,32 @@ function SlideCarousel() {
   ];
 
   return (
-    <div className="relative w-full pt-28">
-      <Carousel
-        plugins={[plugin.current]}
-        className="w-full "
-       
-      >
+    <div className="relative w-full  h-full pt-14">
+      <Carousel plugins={[plugin.current]} className="w-full">
         <CarouselContent>
           {imagesSlide.map((i) => (
             <CarouselItem key={i.id}>
               <Card className="rounded-none shadow-none w-full bg-transparent">
                 <div
-                  className="flex md:min-h-[60vh] md:max-h-[60vh] h-[300px]  md:bg-cover bg-center bg-contain bg-no-repeat items-center justify-center p-6"
+                  className="w-full h-[300px] md:h-[600px] bg-cover bg-center bg-no-repeat"
                   style={{ backgroundImage: `url(${i.src.src})` }}
                 />
               </Card>
             </CarouselItem>
           ))}
-        </CarouselContent>
-        {" "}
+        </CarouselContent>{" "}
         <CarouselPrevious className="absolute left-4 top-2/4 transform -translate-y-1/2 z-20 bg-black/50 text-white p-3 rounded-full" />
         <CarouselNext className="absolute right-4 top-2/4  transform -translate-y-1/2 z-20 bg-black/50 text-white p-3 rounded-full" />
       </Carousel>
-      <div className="hidden md:flex w-full justify-center m-auto text-center md:mb-10" >
-          <Image
-            src={imgBaseSlider}
-            width={1000}
-            height={1000}
-            alt="base"
-            className="w-full"
-          />
-        </div>
+      <div className="hidden md:flex w-full justify-center m-auto text-center md:mb-10">
+        <Image
+          src={imgBaseSlider}
+          width={1000}
+          height={1000}
+          alt="base"
+          className="w-full"
+        />
+      </div>
     </div>
   );
 }
